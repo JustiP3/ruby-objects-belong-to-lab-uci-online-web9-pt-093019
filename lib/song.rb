057a3945719a42bc6564d0f16d76cc41c5a619a3
Justin Phillips <justinphil02@yahoo.com>
2019-11-02 23:48:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,5 @@
 class Song
 attr_accessor :title, :artist
 
-def initialize(title, artist)
-  @artist = artist
-  @title = title
-end #end of method
 
-end #end of song class 
+end #end of song class
